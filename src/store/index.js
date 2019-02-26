2dcdcc7f06cf081d5a1dfac0a8e5c912b2439f3d
vaoooglu <vaoooglu.dn@gmail.com>
2019-02-26 12:26:31 +0300

    del_vue-axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from 'axios';
-import VueAxios from 'vue-axios'
- 
-Vue.use(VueAxios, axios);
+
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
